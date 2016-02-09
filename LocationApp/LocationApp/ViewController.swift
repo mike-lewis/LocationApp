b27bb8a0bed0528b61857eebc2d73bf7b6572704
Mike Lewis <a00929016@btechgames-imac-03.local>
2016-02-09 10:37:02 -0800

    initial commit

---
+++ LocationApp/LocationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationApp
+//
+//  Created by Mike Lewis on 2016-02-09.
+//  Copyright © 2016 Mike Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
